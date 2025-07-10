4b9d3da2143e16d90510253dbfd56702f4e6088c
Nightre <nightscratchs@gmail.com>
2025-07-10 15:33:09 +0800

    按倒

--- lumin-client/src/stores/user.ts
+++ lumin-client/src/stores/user.ts
@@ -9,7 +9,7 @@ export interface IUser {
 export const useUserStore = defineStore('user', () => {
   const token = ref<string | null>(localStorage.getItem("token"))
   const user = ref<IUser | null>()
-  const isLogin = ref(localStorage.getItem("isLogin") == "true")
+  const isLogin = ref(false)
 
   function setToken(newToken: string) {
     token.value = newToken
